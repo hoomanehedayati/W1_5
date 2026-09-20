09ecb5bb7e4891c505358332239524c4a48e9f5d
hooman <hoomanhedayati474@gmail.com>
2026-09-20 13:21:16 +0300

    wall area calculation

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 print("Calculate the area of a wall.")
 Feed=input("Enter the width in meters: ")
-Width=float(Feed)
+Width=int(Feed)
 Feed=input("Enter the height in meters: ")
-Height=float(Feed)
-print(f'The width is {Width} m and height is {Height} m.')
+Height=int(Feed)
+print(f"Width is {Width} m and height is {Height} m.")
 Area=Width*Height
-print(f'The wall will be {Area} square meters.')
-
-
-
+print(f"The wall will be {Area} square meters.")
